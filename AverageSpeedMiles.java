d96c1583d918ca4e9843089c7a00e124ae9a7fd5
zlatedam <42626657+zlatedam@users.noreply.github.com>
2022-04-06 22:49:09 +1000

    todays work

---
+++ AverageSpeedMiles.java
@@ -0,0 +1,15 @@
+// Exercise 1.10 
+
+public class AverageSpeedMiles {
+   
+   public static void main(String [] arg) {
+
+   // Find out how many Miles are in 15KM
+   System.out.println( 15 * 1.6 );
+
+   //Find out the average speed per hour
+   System.out.println((24.0/50.5) * 60); 
+
+  }
+
+}
