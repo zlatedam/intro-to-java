e414c5ac7f24cd1a0bfb94f2ec377cd6f7f8ebcf
zlatedam <zdamc11@gmail.com>
2022-04-24 06:17:45 +1000

    not working

---
+++ Chpt03/Output_3_5_2.java
@@ -0,0 +1,16 @@
+public class Output_3_5_2 {
+  public static void main(String [] args) {
+  
+  int x = 2;
+  int y = 3;
+
+  if (x > 2)
+  if (y > 2) {
+  int z = x + y;
+  System.out.println("z is " + z);
+  }
+  else 
+    System.out.println("x is " + x);
+
+  }
+}
